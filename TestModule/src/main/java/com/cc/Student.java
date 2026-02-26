1b00f8abcf8c4450cf3afefb8ca9a5c3e98df723
wcz <850567223@qq.com>
2026-02-26 16:38:26 +0800

    26-02-25_wcz_第2次提交-增加Student类，main打印

---
+++ TestModule/src/main/java/com/cc/Student.java
@@ -0,0 +1,14 @@
+package com.cc;
+
+/**
+ * @ClassName Student
+ * @Description 添加描述
+ * @Author Wang Chunzheng
+ * @LastChangeDate 2026/2/26 16:34
+ * @Version v1.0
+ */
+
+public class Student {
+    private String name;
+    private int age;
+}
