652753fd62e929903abe984ddcdd3d8068a652d1
wcz <850567223@qq.com>
2026-02-26 17:06:36 +0800

    26-02-25_wcz_第3次提交-增加Person类及其属性

---
+++ TestModule/src/main/java/com/cc/Person.java
@@ -0,0 +1,13 @@
+package com.cc;
+
+/**
+ * @ClassName Person
+ * @Description 添加描述
+ * @Author Wang Chunzheng
+ * @LastChangeDate 2026/2/26 17:04
+ * @Version v1.0
+ */
+
+public class Person {
+    private String name;
+}
